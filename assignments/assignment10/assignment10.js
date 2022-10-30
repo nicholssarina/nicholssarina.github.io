33f8fef5655a6d17008ed4334e52b3a221b5f748
nicholssarina <sani1463@colorado.edu>
2022-10-29 18:25:53 -0600

    figured it out assignment 10

---
+++ assignments/assignment10/assignment10.js
@@ -0,0 +1,64 @@
+document.body.innerHTML = "<h1>ASSIGNMENT 10</h1>";
+let companyData=
+//Q2
+  [
+    {"companyName": "Tech Stars",
+    "website": "www.techstars.site",
+    "employees": [
+       //Q1
+      {
+        "name": "Sam",
+        "department": "Tech",
+        "designation": "Manager",
+        "salary": ["40000"],
+        "raiseEligible": "true"
+      },
+      {
+        "name": "Mary",
+        "department": "Finance",
+        "designation": "Trainee",
+        "salary": ["18500"],
+        "raiseEligible": "true"
+      },
+      {
+        "name": "Bill",
+        "department": "HR",
+        "designation": "Executive",
+        "salary":["21200"],
+        "raiseEligible": "false"
+      },
+      //Q3
+      {
+        "name": "Anna",
+        "department": "Tech",
+        "designation": "Executive",
+        "salary": ["25600"],
+        "raiseEligible": "true"
+      }
+    ]
+   }
+  ]
+  console.log(companyData)
+
+//Q4
+var totalSalaries = companyData[0].employees.reduce((a, b)=> {
+    return a+ parseInt(b.salary[0])
+ }, 0);
+ console.log(totalSalaries);
+
+//Q5
+
+//if employee is raise eligible add 10% to their salary
+
+companyData[0].employees.forEach(function (employees){
+    if (employees.raiseEligible === "true") {
+          employees.salary*=1.10
+    }
+    if (employees.raiseEligible === "true") {
+        employees.raiseEligible = "false"
+    }
+  }
+);
+console.log (JSON.stringify(companyData))
+
+//Q6
