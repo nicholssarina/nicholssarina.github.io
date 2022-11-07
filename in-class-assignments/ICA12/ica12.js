775d389ee2e01e7d85bf15e8d88f7db2f4ca7dc5
nicholssarina <sani1463@colorado.edu>
2022-11-07 11:46:31 -0700

    created assignment 12

---
+++ in-class-assignments/ICA12/ica12.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('.button');
+const output = document.querySelector('.output');
+let phone_content = document.querySelector('.phone');
+
+button.addEventListener('click', updateOutput);
+
+function updateOutput() {
+    output.textContent = phone_content.value;
+}
